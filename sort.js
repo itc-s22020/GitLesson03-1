47f53c244069caec77713d2d94115e6a81bd78ba
itc-s22020 <s22020@std.it-college.ac.jp>
2023-06-05 02:12:54 +0000

    厳密等価演算子(===)を使用するように修正

--- sort.js
+++ sort.js
@@ -3,7 +3,7 @@ console.log(number);
 
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-       if (a == b) {
+       if (a === b) {
            return 0;
        }
        return a < b ? -1 : 1;
